ab16fe315180690f58ecba3c36cb7ee877867299
Tibor Bucsuhazy <bucsuhazy.tibor@gmail.com>
2017-04-06 18:42:35 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Taita on 5. 4. 2017.
+ */
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+// app.use((req, res, next) => {
+//    res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>>');
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: 'Welcome to my website',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to fulfill the request!'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is starting');
+});
+
