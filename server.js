e672cc9312da4d982699ab68d7679d1f5908ebf9
Tibor Bucsuhazy <bucsuhazy.tibor@gmail.com>
2017-04-06 19:25:44 +0200

    added setting for heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -54,7 +55,7 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is starting');
+app.listen(port, () => {
+    console.log(`Server is starting on ${port}`);
 });
 
